a5a963f866363030225034e972376e4d485262fe
novneetnov <novneet346iith@gmail.com>
2017-02-21 13:30:35 +0530

    Add versioning through headers

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This all is just an api app.
 
 ## Stuff todo
 - namespace :api, defaults: { format: :json }
--
+- scope: :module :v1 does not provide v1 in the route.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	namespace :api, constraints: { subdomain: 'api' }, path: '/' do
-		resources :posts 
+		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+			resources :products 
+		end
 	end
 end
