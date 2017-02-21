84709da607c2e713303dc126d0f8bbec173e2257
novneetnov <novneet346iith@gmail.com>
2017-02-21 13:51:29 +0530

    Install Devise and Add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 	namespace :api, constraints: { subdomain: 'api' }, path: '/' do
 		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 			resources :products 
