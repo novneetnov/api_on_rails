f23d271e0f8e6cdbebbf2cf4ced696160bc54580
novneetnov <novneet346iith@gmail.com>
2017-02-21 18:49:42 +0530

    Add show action to users controller and write tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 	namespace :api, constraints: { subdomain: 'api' }, path: '/' do
 		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-			resources :products 
+			resources :users, only: [:show] 
 		end
 	end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -21,6 +24,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 	config.filter_rails_from_backtrace!
 	config.include FactoryGirl::Syntax::Methods
+	config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
