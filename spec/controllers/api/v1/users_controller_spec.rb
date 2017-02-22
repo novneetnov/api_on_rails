ada2c6b69ee442a1e69034ad64d7e71216c76245
novneetnov <novneet346iith@gmail.com>
2017-02-22 13:50:18 +0530

    Add User create method with tests

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 		request.headers['Accept'] = "application/vnd.marketplace.v1"
 	end
 
-	describe "Get #show" do
+	describe "User#show: " do
 		before :each do
 			@user = FactoryGirl.create :user
 			get :show, params: { id: @user.id }, format: :json
@@ -18,5 +18,34 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
 		it { should respond_with 200 }
 
+	end
+
+	describe "User#create: " do
+		context "when User is successfully created" do
+			before :each do
+				@user_attributes = FactoryGirl.attributes_for :user
+				post :create, params: { user: @user_attributes }, format: :json
+			end
+
+			it 'should render the User created in Json format' do
+				user_response = JSON.parse response.body
+				expect(user_response["email"]).to eql @user_attributes[:email]
+			end
+			it { should respond_with 201 }
+		end
+
+		context "when User is not created" do
+			before :each do
+				@invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" }
+				post :create, params: { user: @invalid_user_attributes }, format: :json
+			end
+
+			it { should respond_with 422 }
+			it 'should render json errors on why User could not be created' do
+				user_response = JSON.parse(response.body)
+				expect(user_response).to have_key "errors"
+				expect(user_response["errors"]["email"].first).to eq "can't be blank"
+			end
+		end
 	end
 end
